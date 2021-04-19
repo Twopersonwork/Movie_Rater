7c1afc9dbb8bd4ab327764e1ef80498d914c081a
malavmevada <malavmevada2000@gmail.com>
2021-04-19 22:58:22 +0530

    Added search function

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container} from "react-bootstrap";
 import MovieContainer from "./MovieContainer";
+import Fuse from "fuse.js";
 
 class MovieList extends Component {
   constructor(props) {
@@ -8,31 +9,50 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      matches:[],
+      search:"",
     };
   }
 
   componentDidMount() {
+    console.log(this.props.searchkeyword)
+    if(this.props.searchkeyword.length>0){
+      this.setState({search:this.props.searchkeyword})
+    }
     fetch("http://127.0.0.1:8000/api/movies/", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Token 6622e38254ac5510944a93a53ae3e5d9f6bf5c17",
+        Authorization: "Token 412dd490c43ad5534dd2e1e38a92f9c32d59d62c",
       },
     })
       .then((resp) => resp.json())
-      .then((res) => this.setState({ movies: res }))
+      .then((res) => this.setState({ movies: res },function(){
+        const fuse = new Fuse(this.state.movies, {
+          keys: ["Title"],
+        });
+        const result = fuse.search(this.state.search);
+
+        if (!result.length) {
+          this.setState([]);
+        } else {
+          result.forEach(({item}) => {
+            this.state.matches.push(item);
+          });
+          this.setState({movies:this.state.matches});
+        }
+      }))
       .catch((error) => console.log(error));
   }
 
   render() {
     return (
       <div>
-        <h1>Hii</h1>
-        <h1>Hello</h1>
-        <h1>World</h1>
+        <h1>Hello Django</h1>
+        <h2>Hello react</h2>
         <Container className="pl-3">
           {this.state.movies.map((movie) => (
-            <MovieContainer movie={movie} />
+            <MovieContainer key={movie.id} movie={movie} />
           ))}
         </Container>
       </div>

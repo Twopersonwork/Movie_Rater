1757a7177f26f4508be21b2cc69935a27d259b21
malavmevada <malavmevada2000@gmail.com>
2021-04-26 22:44:02 +0530

    pulled from prachi

--- Movie_Rater_backend/Movie_Rater/settings.py
+++ Movie_Rater_backend/Movie_Rater/settings.py
@@ -93,15 +93,15 @@ WSGI_APPLICATION = 'Movie_Rater.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'moviedb',
-#         'USER': 'postgres',
-#         'PASSWORD': '556232119',
-#         'HOST': '127.0.0.1',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviedb',
+        'USER': 'postgres',
+        'PASSWORD': '556232119',
+        'HOST': '127.0.0.1',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -114,16 +114,16 @@ WSGI_APPLICATION = 'Movie_Rater.wsgi.application'
 #     },
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'de14kodult5i6t',
-        'USER': 'gmxhpxbvbepiwb',
-        'PASSWORD': 'b0bda6036d6b3ed3df23b17d8c972cb91965cd56d9874cf5e7caa5e52f6b82fa',
-        'HOST': 'ec2-3-212-75-25.compute-1.amazonaws.com',
-        'PORT': '5432',
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'de14kodult5i6t',
+#         'USER': 'gmxhpxbvbepiwb',
+#         'PASSWORD': 'b0bda6036d6b3ed3df23b17d8c972cb91965cd56d9874cf5e7caa5e52f6b82fa',
+#         'HOST': 'ec2-3-212-75-25.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     },
+# }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
